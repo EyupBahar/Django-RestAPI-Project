593029d72dc12107551460684f9166ae49f28460
7204-OMER <Eyupbhr24@gmail.com>
2021-08-25 22:02:29 +0300

    add changes

--- src/student_api/views.py
+++ src/student_api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
+from .models import Student
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse('<h1>API Page</h1>')
