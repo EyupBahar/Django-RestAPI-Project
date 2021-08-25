99819b4e4ec552dd9080e9fd1f428f48e09a0e65
7204-OMER <Eyupbhr24@gmail.com>
2021-08-25 23:52:31 +0300

    add urls.py

--- src/student_api/urls.py
+++ src/student_api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import home
+from .views import home, manual_api, student_list_api, student_list_api2, student_add_api, student_api
 
 urlpatterns = [
-  path('', home),
+    path('', home),
+    # path('student/', manual_api),
+    path('list/', student_list_api),
+    path('list2/', student_list_api2),
+    path('add/', student_add_api),
+    path('student/', student_api),
+
 ]
